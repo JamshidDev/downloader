1eb24696dc80d5e9dcce56992fe9278264391ef2
JamshidDev <raximovj662@gmail.com>
2024-08-15 18:15:18 +0500

    fixed service index file

--- frontend/dashboard/src/service/index.js
+++ frontend/dashboard/src/service/index.js
@@ -19,7 +19,7 @@ instance.interceptors.request.use(function (config) {
 instance.interceptors.response.use(
     response => response,
     error => {
-        if(error.response.status==401){
+        if(error.response?.status==401){
             router.push("/login")
         }
         return Promise.reject(error)
